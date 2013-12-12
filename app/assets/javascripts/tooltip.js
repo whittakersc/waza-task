3c2adc1be5df6103009f164509387c131acf3bcc
elliottm <m_elliott@live.com>
2013-12-12 19:16:44 +0000

    a bit of tooltip

---
+++ app/assets/javascripts/tooltip.js
@@ -0,0 +1,10 @@
+
+$(".button").mouseover(function() {
+    $(".icon-caricon").tooltip();
+    console.log('hello');
+  });
+
+$(".new-task").mouseover(function() {
+    $(".button").tooltip();
+    console.log('hello');
+  });
