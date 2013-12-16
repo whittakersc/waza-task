7091a690e040e5e41579ce7b45dc7eedf5c3f81d
elliottm <m_elliott@live.com>
2013-12-16 17:55:51 +0000

    before pull

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -11,12 +11,23 @@ FactoryGirl.define do
           status "To do"
           type "Bau"
           report_tag "Profit and Loss"
+          due_date "#{(Time.now + 4.days)}"
+          time "COB"
+          expected_hrs "3"
   end
   factory :task_project, class: Project do
           description "Deliver a report"
           owner "Andrew"
           status "To do"
           type "Project"
+          report_tag "Profit and Loss"
+          due_date "2013-12-21 00:00:00"
+          time "COB"
+          expected_hrs "3"
           requested_by "Customer"
+  end
+
+  factory(:comment) do
+    description "Goooooo Makers"
   end
 end
